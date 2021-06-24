0b863f532ba9d9b35e6dc0c065895ed6161268c7
Marcos Paulo Marques Corrêa <marcos_barker@hotmail.com>
2021-06-24 16:29:14 -0300

    fix: swegger

--- src/main/java/br/org/serratec/backend/ecommerce/config/SpringFoxConfig.java
+++ src/main/java/br/org/serratec/backend/ecommerce/config/SpringFoxConfig.java
@@ -19,7 +19,7 @@ public class SpringFoxConfig {
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
-				.apis(RequestHandlerSelectors.basePackage("backend.ecommerce.controller"))
+				.apis(RequestHandlerSelectors.any())
 				.paths(PathSelectors.any())
 				.build()
 				.apiInfo(apiInfo());
