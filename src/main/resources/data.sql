795fd9d332343e7c880c82c8c55690959e79672e
Marcos Paulo Marques Corrêa <marcos_barker@hotmail.com>
2021-06-24 01:06:53 -0300

    chore: bd

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -33,7 +33,7 @@ insert into foto (id, nome, mimetype, data) values
    (7, 'fitacrepe.jpg',          'image/jpeg', FILE_READ('classpath:fotos/fitacrepe.jpg')),
    (8, 'mousepad.jpg',           'image/jpeg', FILE_READ('classpath:fotos/mousepad.jpg')),
    (9, 'batalhaapocalipse.jpg',  'image/jpeg', FILE_READ('classpath:fotos/batalhaapocalipse.jpg')),
-   (10,'cadeiraRazer.jpg',       'image/jpeg', FILE_READ('classpath:fotos/cadeiraRazer.jpg'));
+   (10,'cadeirarazer.jpg',       'image/jpeg', FILE_READ('classpath:fotos/cadeirarazer.jpg'));
    
 insert into produto (id, nome, descricao, qtd_estoque, valor, id_categoria, id_funcionario, data_fabricacao, id_foto) values
    (1, 'Cadeira bx9',             'cadeira ergonomica confortavel',   3 ,  850.00, 2, 3, '2019-10-01', 1),
@@ -45,7 +45,8 @@ insert into produto (id, nome, descricao, qtd_estoque, valor, id_categoria, id_f
    (7, 'Fita Crepe',              'Fita crepe simples',              33 ,    1.30, 2, 1, '2012-12-15', 7),
    (8, 'Mousepad',                'Mousepad estilizado com foto',    13 ,   25.00, 1, 1, '2020-01-04', 8),
    (9, 'A Batalha do Apocalipse', 'Melhor livro que voce deve ler',  42 ,   55.00, 3, 2, '2009-01-21', 9),
-   (10, 'Cadeira Gamer Razer Tarok Essential', 'RAZER ™ EDITION Projetada pela ZEN', 8 , 1784.00, 2, 3, '2020-01-21', 10);
+   (10, 'Cadeira Gamer Razer Tarok Essential', 'RAZER EDITION Projetada pela ZEN', 8 , 1784.00, 2, 3, '2020-01-21', 10);
+
 insert into pedido (id, id_cliente, data_pedido, status) values 
   ( 1, 1, '2020-08-30 20:10:10', 4),
   ( 2, 1, '2020-09-10 12:13:12', 2),
@@ -65,7 +66,3 @@ insert into pedido_item  (id_pedido, id_produto, quantidade, valor) values
   ( 4, 9, 1, 60.00),
   ( 5, 4, 2, 65.90),
   ( 5, 5, 1, 3850.00);
-  
-
-
-
