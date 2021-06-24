e197f2b5548bec14026cf81e8c8525fb2ba0572d
Marcos Paulo Marques Corrêa <marcos_barker@hotmail.com>
2021-06-24 15:15:39 -0300

    docs: spring

--- src/main/java/br/org/serratec/backend/ecommerce/config/SpringFoxConfig.java
+++ src/main/java/br/org/serratec/backend/ecommerce/config/SpringFoxConfig.java
@@ -3,8 +3,10 @@ package br.org.serratec.backend.ecommerce.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,9 +19,20 @@ public class SpringFoxConfig {
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
-				.apis(RequestHandlerSelectors.any())
+				.apis(RequestHandlerSelectors.basePackage("br.org.serratec.backend.ecommerce.controller"))
 				.paths(PathSelectors.any())
 				.build();
 	}
+	
+	@SuppressWarnings("unused")
+	private ApiInfo apiInfo() {
+		ApiInfo apiInfo = new ApiInfoBuilder()
+				.title("Documentação ZeroCommerce")
+				.description("API desenvolvida e consumida pelo site ZEROCOMMERCE")
+				.license("Apache License Version 2.0").version("3.0.0")
+				.build();
 
+		return apiInfo;
+	}
+	
 }
